447add86c2dd6d0691dd0c8a9d867fc73100a593
Ramadan3316 <104595014+Ramadan3316@users.noreply.github.com>
2022-05-04 23:08:52 -0300

    Problemas de JS

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 // Para copiar `` b n @
+const alumnos =[{
+    nombre:"Jorgito",
+    email:"jorgito@gmail.com",
+    materia:"Matematica"
+},{
+    nombre:"Cofla",
+    email:"cofla@gmail.com",
+    materia:"Literatura"
+},{
+    nombre:"Juan",
+    email:"juan@gmail.com",
+    materia:"Programacion"
+}]
 
+for(alumno in alumnos){
+    console.log(alumnos[alumno]);
+} 
