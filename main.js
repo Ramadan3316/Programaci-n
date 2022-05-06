ca56fb414797040734507f078e3d38f8ccc5219b
Ramadan3316 <104595014+Ramadan3316@users.noreply.github.com>
2022-05-06 15:58:27 -0300

    calculadora

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 // Para copiar `` b n @
 let multiplicar = document.querySelector(".multiplicar");
-multiplicar.addEventListener("click")
+let display = document.querySelector(".calculadora__display")
+multiplicar.addEventListener("click",()=>{
+    cut();
+})
+const cut = ()=>{
+    for(num in display){
+        while(display[num]!= "X"){
+            console.log(display[num]);
+        }
+    }
+}
