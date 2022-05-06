d9b1954618e98170827aa0e7cd8b49889c01edab
Ramadan3316 <104595014+Ramadan3316@users.noreply.github.com>
2022-05-06 08:15:12 -0300

    Calculadora

---
+++ Problemas/Problema 11.js
@@ -0,0 +1,46 @@
+// Para copiar `` b n @
+const alumnos =[{
+    nombre:"Jorgito",
+    email:"jorgito@gmail.com",
+    materia:"Matematica"
+},{
+    nombre:"Cofla",
+    email:"cofla@gmail.com",
+    materia:"Literatura"
+},{
+    nombre:"Juan",
+    email:"juan@gmail.com",
+    materia:"Programacion"
+}]
+
+let utton = document.querySelector(".button")
+
+for(alumno in alumnos){
+    let datos = alumnos[alumno];
+    let nombre = datos["nombre"];
+    let email = datos["email"];
+    let materia = datos["materia"];
+    let html = `
+            <div class="gridContainer__gridItem nombre">${nombre}</div>
+            <div class="gridContainer__gridItem email">${email}</div>
+            <div class="gridContainer__gridItem materia">${materia}</div>
+            <div class="gridContainer__semana semana">
+                <select class="gridContainer__semanaElegida gridContainer__gridItem">
+                    <option value="Semana 1">Semana 1</option>
+                    <option value="Semana 2">Semana 2</option>
+                </select>
+            </div>`
+        document.querySelector(".gridContainer").innerHTML += html;
+        console.log(html);
+}
+utton.addEventListener("click",()=>{
+    let elementos = document.querySelectorAll(".semana");
+    let semanaElegida = document.querySelectorAll(".gridContainer__semanaElegida")
+    for(elemento in elementos){
+        let semana = elementos[elemento];
+        semana.innerHTML = semanaElegida[elemento].value;
+
+    }
+})
+
+
--- main.js
+++ main.js
@@ -1,18 +1,3 @@
 // Para copiar `` b n @
-const alumnos =[{
-    nombre:"Jorgito",
-    email:"jorgito@gmail.com",
-    materia:"Matematica"
-},{
-    nombre:"Cofla",
-    email:"cofla@gmail.com",
-    materia:"Literatura"
-},{
-    nombre:"Juan",
-    email:"juan@gmail.com",
-    materia:"Programacion"
-}]
-
-for(alumno in alumnos){
-    console.log(alumnos[alumno]);
-} 
+let multiplicar = document.querySelector(".multiplicar");
+multiplicar.addEventListener("click")
