ca847d4756554ccae576bd2133ce8efdf2b58be5
Ramadan3316 <104595014+Ramadan3316@users.noreply.github.com>
2022-05-07 19:44:35 -0300

    Arquitectura & calculadora

---
+++ Arquitectura/script.js
@@ -0,0 +1,8 @@
+function ver() {
+    document.querySelector(".nav__flex-redesSub").style.display="block";
+    document.querySelector(".nav__flex-redesSub").style.transition="all 2s";
+    }
+function ocultar() {
+    document.querySelector(".nav__flex-redesSub").style.display="none";
+    document.querySelector(".nav__flex-redesSub").style.transition="all 2s";
+    }
