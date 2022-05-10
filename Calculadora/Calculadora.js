cc164fcd3a674e16b811f9dbfe642af5550a97a7
Ramadan3316 <104595014+Ramadan3316@users.noreply.github.com>
2022-05-09 23:06:56 -0300

    calculadora

--- Calculadora/Calculadora.js
+++ Calculadora/Calculadora.js
@@ -1,8 +1,13 @@
 // Para copiar `` b n @
 let multiplicar = document.querySelector(".multiplicar");
+let sumar = document.querySelector(".sumar");
+let dividir = document.querySelector(".dividir");
+let restar = document.querySelector(".restar");
 let display = document.querySelector(".calculadora__displayNum")
 let divDisplay = document.querySelector(".calculadora__display");
 let igual = document.querySelector(".igual");
+let divNumeros = document.querySelector(".numeros");
+
 
 
 function Numeros(){
@@ -45,9 +50,39 @@ function Numeros(){
     return resultado;
 }
 
+for(i=0;i>9;i++){
+   let numeros = document.querySelector(`.num${i}`);
+   console.log(numeros[i]);
+}
+
+multiplicar.addEventListener("click",()=>{
+    display.value =  display.value + "*" ;
+    // console.log(display.innerHTML =  inputValue + "*" )
+})
+sumar.addEventListener("click",()=>{
+    display.value =  display.value + "+" ;
+    // console.log(display.innerHTML =  inputValue + "+" )
+})
+dividir.addEventListener("click",()=>{
+    display.value =  display.value + "/" ;
+    // console.log(display.innerHTML =  inputValue + "/" )
+})
+restar.addEventListener("click",()=>{
+    display.value =  display.value + "-" ;
+    // console.log(display.innerHTML =  inputValue + "*" )
+})
+
+
 igual.addEventListener("click",()=>{
+    divDisplay.classList.add("resultado");
     numeros = Numeros();  
     console.log(numeros);
-    divDisplay.innerHTML = numeros;
-    divDisplay.classList.add("resultado");
+    divDisplay.innerText = numeros;
+    let nashe = divNumeros.value;
+    while(i!=9){
+        i++;
+        let num = document.querySelector(`.num${i}`);
+        console.log(num);
+    }
+    
 })
