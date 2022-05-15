cbfd7bf0333e8c2ce095d3a76b6340647f2c06fe
Ramadan3316 <104595014+Ramadan3316@users.noreply.github.com>
2022-05-15 17:55:46 -0300

    domingo

--- Arquitectura/script.js
+++ Arquitectura/script.js
@@ -1,8 +1,41 @@
+const image = document.querySelector(".imagePoint")
+const slider = document.querySelector(".imageExample");
+const touch = document.querySelector(".touch");
+const next = document.querySelector(".next")
+
+image.addEventListener("click",()=>{
+    slider.style.display = "inline-block";
+    console.log("se presiono la imagen");
+})
+
+touch.addEventListener("click",()=>{ 
+    slider.style.display = "none";
+    console.log("Se toco el volver");
+})
+
+next.addEventListener("click",()=>{
+    console.log("Se toco el next")
+})
+
 function ver() {
     document.querySelector(".nav__flex-redesSub").style.display="block";
     document.querySelector(".nav__flex-redesSub").style.transition="all 2s";
-    }
+}
 function ocultar() {
     document.querySelector(".nav__flex-redesSub").style.display="none";
     document.querySelector(".nav__flex-redesSub").style.transition="all 2s";
+}
+
+function showSlides() {
+    var slideIndex = 0;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
+    slideIndex++;
+    if(slideIndex > slides.length) slideIndex = 1
+    slides[slideIndex-1].style.display = "block";
+}
+showSlides();
+
+
