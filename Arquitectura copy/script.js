84727376e917e3c56ab83cbcb22b0d5af76e5ccd
Ramadan3316 <104595014+Ramadan3316@users.noreply.github.com>
2022-05-27 20:39:44 -0300

    arquitectura ashe

--- Arquitectura copy/script.js
+++ Arquitectura copy/script.js
@@ -1,16 +1,12 @@
 const menuLogo = document.querySelector(".nav__menuLogo");
 const menu = document.querySelector(".nav__menu")
 const salir = document.querySelector(".salirMenu");
-const home = document.querySelector(".homeImage")
+const home = document.querySelector(".homeImage");
+const body = document.querySelector(".body");
 menuLogo.addEventListener("click",()=>{
     body.style.backgroundColor = "rgba(0,0,0,0.7)"
     menu.style.display = "block";
     console.log("se presiono el icono")
-    // menu.animate([
-    //     {width: "5vw"},
-    //     {width:"20vw", offset:0.5},
-    //     {width:"35vw"}
-    // ],100)
 })
 
 salir.addEventListener("click",()=>{
@@ -18,20 +14,16 @@ salir.addEventListener("click",()=>{
     menu.style.display = "none";
     menuLogo.style.marginRight = "10px"
     console.log("se presiono el icono de salida");
-    // menu.animate([
-    //     {width: "35vw"},
-    //     {width:"20vw", offset:0.5},
-    //     {width:"5vw"}
-    // ],5000)
 })
 
 const animacion = ()=>{
     for(let i=0;i>25;i++){
         let figura = document.createElement('span');
         
-        
         homeImage.appendChild(figura);
     }
 }
 
 animacion();
+
+
--- Arquitectura/script.js
+++ Arquitectura/script.js
@@ -1,43 +1,53 @@
-const image = document.querySelector(".imagePoint")
-const slider = document.querySelector(".imageExample");
-const touch = document.querySelector(".touch");
-const next = document.getElementById("next");
-const nav = document.querySelector("#nav");
+// const image = document.querySelector(".imagePoint")
+// const slider = document.querySelector(".imageExample");
+// const touch = document.querySelector(".touch");
+// const next = document.getElementById("next");
+// const nav = document.querySelector("#nav");
 
-image.addEventListener("click",()=>{
-    slider.style.display = "flex";
-    // slider.style.position = "absolute"
-    nav.style.display = "none";
-    console.log("se presiono la imagen");
-    slider.style.animation = "slider 5s fowards";
-})
+// image.addEventListener("click",()=>{
+//     slider.style.display = "flex";
+//     // slider.style.position = "absolute"
+//     nav.style.display = "none";
+//     console.log("se presiono la imagen");
+//     slider.style.animation = "slider 5s fowards";
+// })
 
-touch.addEventListener("click",()=>{ 
-    slider.style.display = "none";
-    nav.style.display = "block";
-    console.log("Se toco el volver");
-    slider.style.animation = "slider 5s fowards";
-})
+// touch.addEventListener("click",()=>{ 
+//     slider.style.display = "none";
+//     nav.style.display = "block";
+//     console.log("Se toco el volver");
+//     slider.style.animation = "slider 5s fowards";
+// })
 
-function ver() {
-    document.querySelector(".nav__flex-redesSub").style.display="block";
-    document.querySelector(".nav__flex-redesSub").style.transition="all 2s";
-}
-function ocultar() {
-    document.querySelector(".nav__flex-redesSub").style.display="none";
-    document.querySelector(".nav__flex-redesSub").style.transition="all 2s";
-}
+// function ver() {
+//     document.querySelector(".nav__flex-redesSub").style.display="block";
+//     document.querySelector(".nav__flex-redesSub").style.transition="all 2s";
+// }
+// function ocultar() {
+//     document.querySelector(".nav__flex-redesSub").style.display="none";
+//     document.querySelector(".nav__flex-redesSub").style.transition="all 2s";
+// }
 
-function showSlides() {
-    var slideIndex = 0;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+// function showSlides() {
+//     var slideIndex = 0;
+//     var slides = document.getElementsByClassName("mySlides");
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if(slideIndex > slides.length) slideIndex = 1
+//     slides[slideIndex-1].style.display = "block";
+// }
+// showSlides();
+
+
+let clickImage = []
+function selectImage(index){
+    for(let i = 0; i < 5; i++){
+        clickImage[i] = document.querySelector(`.imagesGrid__img${i}`);
     }
-    slideIndex++;
-    if(slideIndex > slides.length) slideIndex = 1
-    slides[slideIndex-1].style.display = "block";
+    imageClass = clickImage[index].classList;
+    console.log("las clases de las imagenes son "+imageClass);
+
+
 }
-showSlides();
-
-
