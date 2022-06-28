01791fbc04a22f1fd0ce8d7f20a389cd1381a340
Ramadan3316 <104595014+Ramadan3316@users.noreply.github.com>
2022-06-28 16:59:35 -0300

    TATEIT

--- main.js
+++ main.js
@@ -10,19 +10,41 @@ let turn = 0;   // tuen 0 Usuario 1 PC
 
 const boardContainer = document.querySelector("#board");
 const playerDiv = document.querySelector("#player");
-
 startGame();
 
 function startGame() {
     renderBoard();
     turn = Math.random() <= 0.5 ? 0 :1;
-    renderCurrectPlayer();
+    renderCurrentPlayer();
     playerPlay();
 }
+function winning(cell,i){
+
+}
 function playerPlay(){
+    const cells = document.querySelectorAll(".cell");
+    cells.f
+    forEach((cell,i) =>{
+        cell.addEventListener("click", () =>{
+            if(turn == 0 && cell.textContent == ""){
+                board[i] = "X";
+                cell.textContent = "X";
+                winning(cell,i);
+                turn = 1;
+                renderCurrentPlayer();
+            }
+            else if (turn == 1 && cell.textContent == "") {
+                board[i] = "O";
+                cell.textContent = "O";
+                winning(cell,i); 
+                turn = 0;
+                renderCurrentPlayer();
+            }
+        });   
+    });
 }
 
-function renderCurrectPlayer(){
+function renderCurrentPlayer(){
     playerDiv.textContent = `${turn == 0 ? "Player 1 turn" : "Player 2 turn"}`
 }
 
