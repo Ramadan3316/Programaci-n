51c2406fa1c0fbe96e381638279ea093f345df2e
Ramadan3316 <104595014+RamiroCS-hub@users.noreply.github.com>
2022-07-16 16:23:32 -0300

    API

--- APIrest/src/Routes/datos.js
+++ APIrest/src/Routes/datos.js
@@ -6,12 +6,34 @@ const Dato = require("../models/data")
 router.get("/", async (req,res)=>{
     console.log("Printeando data...")
     try {
-        const arrayDatos = await Dato.find();
+        arrayDatos = await Dato.find();
         console.log(arrayDatos);
+        res.json(arrayDatos);
+        // res.render("Home",{
+        //     arrayDatos : arrayDatos
+        // });
     } catch(e){
         console.log(e);
-    }
-    // console.log("La res es: ",res);
+    } 
 })
+
+router.post("/", (req,res)=>{
+    const {name,prize} = req.body; 
+    if(name && prize) {
+        res.send("recibido");// Envío mensaje a que hizo el request
+        console.log("Formato Correcto");
+        const newData = {...req.body}; // Creo un newObject con todas las propiedades del req
+        Dato.insertMany(newData);//Inserto en la base de datos
+    }
+    else{
+        res.status(500).json({error:"Ah ocurrido un error"});
+    }
+});
+
+router.delete("/:name",(req,res)=>{
+    const {name} = req.params.name;
+    Dato.deleteMany({name:name})
+})
+
 
 module.exports = router;
--- APIrest/src/main.js
+++ APIrest/src/main.js
@@ -1,32 +1,34 @@
 // Para copiar `` b n @
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
 const mongoose = require("mongoose");
+const morgan = require("morgan")
 
 //Conexiones
 const user ="Ramadan3316";
 const password="2oKJh4L5ua4cwaR5";
 const nameD = "TEST";
-const url=`mongodb+srv://${user}:${password}@mongodata.vxtbq.mongodb.net/${nameD}?retryWrites=true&w=majority`;
+const url=`mongodb+srv://Ramadan3316:${password}@mongodata.vxtbq.mongodb.net/${nameD}?retryWrites=true&w=majority`;
+
 mongoose.connect(url,
     { useNewUrlParser: true, useUnifiedTopology: true } //Para que no nos tiren errores en la consola
 ).then(()=> console.log("Mongo Conectado"))
     .catch((e)=> console.log("El eror es: ",e));
 
 //Configuraciones
-app.set("port", process.env.PORT || 3000); // Si el servidor me da un puerto uso ese si no el 300
+app.set("port",process.env.PORT || 3000); // Si el servidor me da un puerto uso ese si no el 3000
+app.set("view engine","ejs");
+app.set("views",__dirname+'/views');
 
 //Middleware(Ejecuta antes del programa)
-// app.use(morgan("dev")); // Para ver las peticiones en consola
 app.use(express.urlencoded({extended: false})) //Para recibir datos sencillos de formularios
 app.use(express.json()); // Para poder soportar el formato JSON
-
+app.use(morgan("dev"));
 //Rutas
 app.use(require("./Routes/index"));
 app.use("/api/datos",require("./Routes/datos"));
 
-//Starting server
+//INICIO
 app.listen(app.get("port"),()=>{
     console.log("Conected");
 })
