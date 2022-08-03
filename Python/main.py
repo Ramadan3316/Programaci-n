ec6e1485c09a466863e554141bdd07a32e25bd83
Ramadan3316 <104595014+RamiroCS-hub@users.noreply.github.com>
2022-08-03 07:08:10 -0300

    pygames

--- Python/main.py
+++ Python/main.py
@@ -1,24 +1,32 @@
 #b n N
-import pygame, sys;
+import pygame, sys, time;
+
+pygame.init();
 xScreen = 800;
 yScreen = 600;
 surf = pygame.display.set_mode((xScreen,yScreen));
 screen = pygame.display;
-x=0;
-y=yScreen;
-width=50;
-heigth=100;
-flagUp = True;
-flagDown = True;
 keys = {
     'a':False,
     'd':False,
-    'w':False,
     's':False, 
 }
 
-while 1:
-    rect1 = pygame.rect.Rect((x,y - heigth), (width,heigth))
+class Player:
+    x=10;
+    y=yScreen;
+    
+    def __init__(self,width,height):
+        self.width = width;
+        self.heigth = height;
+    def gravity(self):
+        self.y += 0.5;
+
+player1 = Player(25,50);  
+
+runGame = True;
+
+while runGame:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit();
@@ -26,48 +34,43 @@ while 1:
             if event.key == pygame.K_ESCAPE:
                 sys.exit();
             if event.key == pygame.K_w:
-                keys['w'] = True
-                flagU = True;
+                if player1.y >= yScreen - 5:
+                    player1.y -= 200;
             if event.key == pygame.K_a:
                 keys['a'] = True
-                flagL = True;
             if event.key == pygame.K_s:
                 keys['s'] = True
-                flagD = True;
             if event.key == pygame.K_d:
                 keys['d'] = True
-                flagR = True;  
+                
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_w:
-                keys['w'] = False
-                flagU = True;
             if event.key == pygame.K_a:
                 keys['a'] = False;
-
             if event.key == pygame.K_s:
                 keys['s'] = False
             if event.key == pygame.K_d:
                 keys['d'] = False;
-        if y < 1:
-            flagU = False;
-        if y + heigth > yScreen:
-            flagD = False;
-        if x < 0:
-            flagL = False;
-        if x + width > xScreen:
-            flagR = False;
+        
+            print(player1.y)
 
-    if keys['w'] and flagU:
-        y -= 0.2
-    if keys['a'] and flagL:
-        x -= 0.2
-    if keys['d'] and flagR:
-        x += 0.2;
-    if keys['s'] and flagD:
-        y += 0.2;
+    if player1.y  < yScreen:
+        player1.gravity();
 
+    if keys['a']:
+        if player1.x > 0:
+            player1.x -= 0.2;
 
-    pygame.draw.rect(surf, (0,0,0), (0,0,800,600))
+    if keys['s']:
+        if player1.y  <= yScreen:
+            player1.y += 0.2;
+
+    if keys['d']:
+        player1.x += 0.2;
+        posCam += 10;
+    rect1 = pygame.rect.Rect((player1.x,player1.y - player1.heigth), (player1.width,player1.heigth))
+    rectT = pygame.rect.Rect((200,200), (player1.width,player1.heigth))
+    pygame.draw.rect(surf, (0,0,0), (0,0,xScreen,yScreen))
+    pygame.draw.rect(surf,(255,255,255),rectT)
     pygame.draw.rect(surf,(255,255,255),rect1);
     screen.update();
     
